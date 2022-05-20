96f989875f67b888142e8c36f33b10c710aeb75e
Hussein Taan <hst06@mail.aub.edu>
2022-05-20 23:59:25 +0300

    some html / css to the graphs

--- Application/routes.py
+++ Application/routes.py
@@ -3,13 +3,64 @@ from Application import app
 from flask import render_template, request, session, g, redirect, url_for, flash
 from User import User, login_required
 
+from FigureGenerator import Figure
+from FigureGenerator.Traces.ScatterTrace import ScatterTrace
+from FigureGenerator.Traces.BarTrace import BarTrace
+from FigureGenerator.Traces.LineTrace import LineTrace
+from FigureGenerator.Traces.ScatterLineTrace import ScatterLineTrace
+from FigureGenerator.Traces.CandleStickTrace import CandleStickTrace
 from DataBaseTools.getCompanies import getCompanies
+
 
 @app.route("/")
 # @login_required    #by uncommenting this,you can force user to sign in to access this page
 def index():
     select = getCompanies()
     return render_template("index.html", options1=select)
+
+
+@app.route("/candleStickChart")
+def candleStickChart():
+    select = getCompanies()
+    name = request.args.get('company')
+    df = getStockPrice(name)
+
+    fig = Figure()
+    fig.set_Title("%s stock history" % (name))
+    fig.set_xLabel("Date")
+    fig.set_yLabel("Price")
+
+    trace = CandleStickTrace()
+    trace.set_name(name)
+    trace.add_X_values(df["Date"].tolist())
+    trace.add_open_values(df["Open"].tolist())
+    trace.add_low_values(df["Low"].tolist())
+    trace.add_high_values(df["High"].tolist())
+    trace.add_close_values(df["Close"].tolist())
+    trace.set_increasing_line_color("green")
+    trace.set_decreasing_line_color("red")
+
+    fig.add_trace(trace)
+
+    fig2 = Figure()
+    fig2.set_xLabel("Date")
+    fig2.set_yLabel("Volume")
+
+    volTrace = BarTrace()
+    volTrace.add_X_values(df["Date"].tolist())
+    volTrace.add_Y_values(df["Volume"].tolist())
+    volTrace.set_color("blue")
+    volTrace.set_name("Volume")
+
+    fig2.add_trace(volTrace)
+
+    return render_template("figures.html",
+                           title="%s stock data" % (name),
+                           figure1=fig.render(),
+                           figure2=fig2.render(),
+                           company=name,
+                           options1=select
+                           )
 
 
 @app.route("/login", methods=('GET', 'POST'))
